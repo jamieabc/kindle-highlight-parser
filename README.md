4c1436c5d1ed7bc25d12c9596a903f797a3db9ae
Aaron Chang <xesuschang@gmail.com>
2019-09-28 15:31:52 +0800

    Squashed commit of 1-parse:
    commit 34b13486c2886debfaf34f2dfe1b7c33012cb081
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Sep 28 15:30:22 2019 +0800
    
        parse kindle highlights

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-This program parses kindle highlights. It searches current directory file of 'My Clippings.txt', parse it and output to 'highlights.txt'
+This program parses kindle highlights. It searches current directory file of `My Clippings.txt`, parse it and output to `kindle.txt`
+
+If it's a mac, kindle clipping file is located under `/Volumes/Kindle/documents/My Clippings.txt`
+
+Note:
+    Currently only test chinese version, not sure if other version with same syntax.
+    If there's any problem, please send out issue and attach the `My Clippings.txt`
+
+Install:
+    `go get -u github.com/jamieabc/kindle-highlight-parser`
 
 Usage:
-    kindle-highlight-parser
+    `kindle-highlight-parser`
---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+)
+
+const (
+	inputFileName      = "My Clippings.txt"
+	outputFileName     = "kindle.txt"
+	highlightSeparator = "=========="
+	bookStartIndex     = 3 // \ufeff
+	positionPrefix     = "- "
+	sentenceStartIndex = 3
+	newLineChars       = "\r\n"
+
+	// "2018年5月3日星期四 下午2:59:59"
+	timeFormat = "(20\\d\\d).*(\\d+).*(\\d+).* (.*)(\\d+):(\\d+):(\\d+)"
+)
+
+type highlight struct {
+	sentence string
+	date     string
+}
+
+type highlights map[string][]highlight
+
+func main() {
+	bytes, err := rawBytes(inputFileName)
+	if nil != err {
+		fmt.Printf("get highlight with error: %s\n", err)
+		return
+	}
+	hs := parse(bytes)
+	writeToOutputFile(hs)
+}
+
+func rawBytes(file string) ([]byte, error) {
+	handle, err := os.Open(file)
+	if nil != err {
+		return nil, err
+	}
+	defer handle.Close()
+
+	bytes, err := ioutil.ReadAll(handle)
+	if nil != err {
+		return nil, err
+	}
+
+	return bytes, nil
+}
+
+func parse(raw []byte) highlights {
+	paragraphs := strings.Split(string(raw), highlightSeparator)
+	hs := make(highlights)
+
+	for _, p := range paragraphs {
+		truncated := removeFirstEmptyLines(p)
+		if 2 >= len(truncated) {
+			continue
+		}
+		bookName, timeStr, sentence := parseParagraph(truncated)
+		combineHighlight(hs, bookName, highlight{
+			sentence: sentence,
+			date:     timeStr,
+		})
+	}
+
+	return hs
+}
+
+func removeFirstEmptyLines(line string) string {
+	for i := 0; i < len(line); i = i + 2 {
+		if newLineChars != line[i:i+2] {
+			return line[i:]
+		}
+	}
+	return ""
+}
+
+func parseParagraph(paragraph string) (bookName string, timeStr string, sentence string) {
+	lines := strings.Split(paragraph, "\r\n")
+	bookName = lines[0][3:]
+	timeStr, err := parseTime(lines[1])
+	if nil != err {
+		fmt.Printf("parse time of %s with error: %s\n", lines[1], err)
+		return
+	}
+	sentence = strings.Join(lines[3:], "\n")
+	return
+}
+
+func combineHighlight(hs highlights, book string, h highlight) {
+	if _, ok := hs[book]; !ok {
+		hs[book] = make([]highlight, 0)
+	}
+	hs[book] = append(hs[book], h)
+}
+
+func parseTime(line string) (string, error) {
+	r, _ := regexp.Compile(timeFormat)
+	matches := r.FindAllString(line, -1)
+	if 1 != len(matches) {
+		return "", errors.New("error parse time format")
+	}
+	return matches[0], nil
+}
+
+func writeToOutputFile(hs highlights) {
+	f, err := os.Create(outputFileName)
+	if nil != err {
+		fmt.Printf("create file %s with error: %s\n", outputFileName, err)
+		return
+	}
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+	for bookName, sentences := range hs {
+		w.WriteString(fmt.Sprintf("%s\n\n", bookName))
+		for _, s := range sentences {
+			str := fmt.Sprintf("\t- %s\n", s.sentence)
+			w.WriteString(str)
+		}
+	}
+	w.Flush()
+}
